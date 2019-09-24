45294f68f02795b5b4399b3eec7f3a1f30794f8c
Chris Ringrose <43569573+CRingrose94@users.noreply.github.com>
2019-09-24 15:36:34 +0100

    Update sequences.py

--- sequences.py
+++ sequences.py
@@ -1,3 +1,26 @@
+import itertools as it
+
+
+def grouper(inputs, n, filler=None):
+    iters = [iter(inputs)] * n
+    return it.zip_longest(*iters, fillvalue=filler)
+
+
+def better_enumerate(option=1):
+
+    letters = ['a', 'b', 'c', 'd', 'e']
+
+    if option == 1:
+        tups = []
+        for i, let in enumerate(letters):
+            tups.append((i, let))
+        return tups
+    elif option == 2:
+        return [(i, let) for i, let in enumerate(letters)]
+    else:
+        return list(zip(it.count(), letters))
+
+    
 def flatten(sequence):
     """
     Given any nested sequences, flatten it to a single sequence
@@ -11,6 +34,9 @@ def flatten(sequence):
 
 
 if __name__ == '__main__':
-    a = flatten([1, [2, 3], [4, [5]]])
+    print(better_enumerate())
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print(list(grouper(nums, 3)))
 
+    a = flatten([1, [2, 3], [4, [5]]])
     print(list(a))
